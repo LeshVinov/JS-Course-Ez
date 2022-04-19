9c9901f9593d85b69163fe51a0d66354207b5ed9
LeshVinov <90595946+LeshVinov@users.noreply.github.com>
2022-04-19 15:17:42 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import timer from './modules/timer'
+import menu from './modules/menu'
+import modal from './modules/modal'
+import calculator from './modules/calculator'
+import formValidate from './modules/formValidate'
+
+
+timer('20 april 2022')
+menu()
+modal()
+calculator()
+formValidate()
