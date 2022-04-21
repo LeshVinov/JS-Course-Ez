efe23de570a664187eab882e3fa3f1366467c252
LeshVinov <90595946+LeshVinov@users.noreply.github.com>
2022-04-21 10:39:07 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import menu from './modules/menu'
 import modal from './modules/modal'
 import calculator from './modules/calculator'
 import formValidate from './modules/formValidate'
+import smoothDwnBtn from './modules/smoothDwnBtn'
 
 
-timer('20 april 2022')
+timer('30 april 2022')
 menu()
 modal()
 calculator()
 formValidate()
+smoothDwnBtn()
